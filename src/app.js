29bd1835b7073265df850c6223dc16cab43361ff
buketh <buket.hepgunay@gmail.com>
2022-05-19 21:50:40 +0200

    updated the time

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = date.getMinutes();
-  let day = date.getDay();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
   let cityELement = document.querySelector("#temperature");
   let temperatureELement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
@@ -26,8 +41,7 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "a6dd9caacfc4a8d8972e0f359cde221a";
+let apiKey = "bf5fc26e216feede5c06b2e88d694cf2";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=a6dd9caacfc4a8d8972e0f359cde221a&units=metric`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
