0212b032e99cec9288158ce428aa9b9812f18121
buketh <buket.hepgunay@gmail.com>
2022-05-20 22:21:11 +0200

    Added relevant icon from the API

--- src/app.js
+++ src/app.js
@@ -22,25 +22,27 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  let cityELement = document.querySelector("#temperature");
-  let temperatureELement = document.querySelector("#temperature");
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  temperatureELement.innerHTML = Math.round(response.data.main.temp);
-  cityELement.innerHTML = response.data.name;
+  let iconElement = document.querySelector("#icon");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "bf5fc26e216feede5c06b2e88d694cf2";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=a6dd9caacfc4a8d8972e0f359cde221a&units=metric`;
+
+let apiKey = "66e6430c68b170d229d336c712c23f43";
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
